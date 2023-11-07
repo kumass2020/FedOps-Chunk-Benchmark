ee675fcd56c9dcfd1fd4285a5f094ee7b05c5472
hoho <kumass2021@gmail.com>
2023-11-07 23:52:37 +0900

    add: seperated clients test

--- quickstart_server.py
+++ quickstart_server.py
@@ -27,12 +27,12 @@ strategy = fl.server.strategy.FedAvg(
 wandb.init(
     entity="hoho",
     # set the wandb project where this run will be logged
-    project="fedops-loss-map",
+    project="fedops-baselines-fedavg",
 )
 
 # Start Flower server
 fl.server.start_server(
     server_address="0.0.0.0:8080",
-    config=fl.server.ServerConfig(num_rounds=5),
+    config=fl.server.ServerConfig(num_rounds=3),
     strategy=strategy,
 )
